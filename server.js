ca5912717dd29ea24085a3f262e604745b7ddd9b
Jonathan Kapuya <113067253+KapuyaJonathan1@users.noreply.github.com>
2024-11-23 22:32:32 -0800

    Fix production deployment

--- server.js
+++ server.js
@@ -196,7 +196,7 @@ app.post('/api/get-info', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
