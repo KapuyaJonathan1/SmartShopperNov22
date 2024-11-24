d2e8d7fca9976921d6d8ecc9ca8560dae5a49006
Jonathan Kapuya <113067253+KapuyaJonathan1@users.noreply.github.com>
2024-11-23 21:51:30 -0800

    Configure for Heroku deployment

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.json({ limit: '50mb' })); // Increase JSON payload limit
 app.use(express.urlencoded({ limit: '50mb', extended: true })); // For form-encoded data
-
+app.use(express.static(path.join(__dirname, 'build')));
 // Google OAuth2 Configuration
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
@@ -128,6 +128,12 @@ app.get('/callback', async (req, res) => {
 // });
 const fetch = require('node-fetch'); // Ensure fetch is available
 
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.post('/api/get-info', async (req, res) => {
   const { prompt } = req.body;
   console.log(prompt);
@@ -189,6 +195,8 @@ app.post('/api/get-info', async (req, res) => {
     res.status(500).json({ error: 'Failed to fetch information from Gemini.' });
   }
 });
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
