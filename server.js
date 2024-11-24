65f05972191e0296250046124f6f7afc8172b5e0
Jonathan Kapuya <113067253+KapuyaJonathan1@users.noreply.github.com>
2024-11-23 22:38:10 -0800

    Fix hardcoded port issue

--- server.js
+++ server.js
@@ -196,7 +196,7 @@ app.post('/api/get-info', async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
