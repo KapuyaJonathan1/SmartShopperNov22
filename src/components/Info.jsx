ec3173526c1cf37bba0f44b30043770f44f15f74
Jonathan Kapuya <113067253+KapuyaJonathan1@users.noreply.github.com>
2024-11-28 22:02:25 -0800

    h

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -19,7 +19,7 @@ function Info({ health, budget, image, store, switchPage }) {
       });
       const food = item.data.information
 
-      if(food === "Not Food.") {
+      if(food === "Not Food") {
         setInformation([
           { information: "Unrecognized Food" },
           { information: "Unrecognized Food" },
